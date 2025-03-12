dfde1db218cdff8b30394f23939be5dff3cf6668
C_11nJxx <c_11njxxsmailbox@sina.com>
2025-03-12 12:05:48 +0800

    Day1(AM) (Webpack dev server)

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const historyApiFallback = require('connect-history-api-fallback');
 
 module.exports = {
     mode: 'development',
@@ -9,6 +10,16 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].[contenthash].js'
+    },
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist')
+        },
+        port: 3000,
+        open: true,
+        hot: true,
+        compress: true,
+        historyApiFallback: true
     },
     module: {
         rules: [
