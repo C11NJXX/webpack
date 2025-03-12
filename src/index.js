1fc343d7c337c578d40a545a87325d3250d447c4
C_11nJxx <c_11njxxsmailbox@sina.com>
2025-03-12 14:06:47 +0800

    Day1(AM) (Finish the joke app)

--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import './styles/main.scss'
 import image from './assets/image.png'
 
 document.getElementById('laughImg').src = image;
-
+document.getElementById('jokeBtn').addEventListener('click', () => {
+    generateJoke();
+});
 generateJoke();
